0021b4fd5d1a153a9899b9a8fb298bba0b52724f
Mariana Santos <marianasferra31@gmail.com>
2025-09-15 10:01:42 -0300

    feat: new model Category (ForeingKey)

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,11 @@ from django.utils import timezone
 # Depois
 # owner (foreign key)
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -19,6 +24,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True) 
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
