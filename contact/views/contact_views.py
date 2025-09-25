157fa21fdb598de062820a2f4175d14ee5b858b4
Mariana Santos <marianasferra31@gmail.com>
2025-09-25 09:09:14 -0300

    feat(view): creating new view

---
+++ contact/views/contact_views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404
+from contact.models import Contact
+
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:12]
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ' 
+    }
+    return render(request, 
+                  'contact/index.html',
+                  context
+                  )
+
+def contact(request, contact_id):
+    #single_contact = Contact.objects.get(pk=contact_id)
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+    return render(request, 
+                  'contact/contact.html',
+                  context
+                  )
+
+def search(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:12]
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ' 
+    }
+    return render(request, 
+                  'contact/index.html',
+                  context
+                  )
+
+
