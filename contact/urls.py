83496f3d2d7d3f09a55a8aba1987c01756254d32
Mariana Santos <marianasferra31@gmail.com>
2025-10-08 16:37:44 -0300

    Refact: ajuste de formatação das urls

--- contact/urls.py
+++ contact/urls.py
@@ -4,6 +4,8 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+    path('search/', views.search, name='search'),  
+    path('', views.index, name='index'),  
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
