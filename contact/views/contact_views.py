75df1d9e304adccf18dd213dc52a41b8e2eb54e1
Mariana Santos <marianasferra31@gmail.com>
2025-10-08 16:38:47 -0300

    Feat: aplicando a paginação na view dos Contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
+from django.core.paginator import Paginator
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:12]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    paginator = Paginator(contacts, 12)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ' 
     }
-    return render(request, 
+    return render(
+                  request, 
                   'contact/index.html',
                   context
                   )
@@ -30,10 +37,24 @@ def contact(request, contact_id):
                   )
 
 def search(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:12]
+    search_value =  request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | 
+                                                        Q(last_name__icontains=search_value) |
+                                                        Q(phone__icontains=search_value) |
+                                                        Q(email__icontains=search_value)).order_by('-id')
+    
+    paginator = Paginator(contacts, 12)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
+        'page_obj': page_obj,
         'contacts': contacts,
-        'site_title': 'Contatos - ' 
+        'site_title': 'Search - ' 
     }
     return render(request, 
                   'contact/index.html',
