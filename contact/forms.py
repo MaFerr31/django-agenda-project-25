eacd03ad427b2c23f8658a1c234a6902d4e1caed
Mariana Santos <marianasferra31@gmail.com>
2025-10-16 12:12:17 -0300

    Feat: owner adicionado

--- contact/forms.py
+++ contact/forms.py
@@ -23,7 +23,9 @@ class ContactForm(forms.ModelForm):
             attrs={
                 'accept': 'image/*',
             }
-        )
+        ),
+
+        required=False
     )
 
     class Meta:
