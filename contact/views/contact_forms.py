af697aea19c4cb7f2dbbab24a48d2b2272a6251b
Mariana Santos <marianasferra31@gmail.com>
2025-10-18 20:24:19 -0300

    fix: update usuário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -42,7 +42,7 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
 
     form_action = reverse('contact:update', args=(contact_id,))
@@ -56,7 +56,7 @@ def update(request, contact_id):
 
         if form.is_valid():
             contact = form.save()
-            return redirect('contact:update', contact_id=contact.pk, owner=request.user)
+            return redirect('contact:update', contact_id=contact.pk)
         
         return render(
             request,
